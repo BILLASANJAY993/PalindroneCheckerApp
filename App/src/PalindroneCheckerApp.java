c0ed9ac6fdd003af6e6f89b4f1868b81d0db664b
BILLASANJAY993 <sb1359@srmist.edu.in>
2026-02-28 13:43:24 +0530

    Sanjay Added  UC4-CharacterArray

--- App/src/PalindroneCheckerApp.java
+++ App/src/PalindroneCheckerApp.java
@@ -1,20 +1,23 @@
 import java.util.Scanner;
 public class PalindroneCheckerApp {
-        public static void main(String[] args) {
-            System.out.println("Palindrome Checker App");
-            System.out.println();
-            Scanner sc = new Scanner(System.in);
-            System.out.print("Input text: ");
-            String text = sc.nextLine();
-            String reversed = "";
-            for (int i = text.length() - 1; i >= 0; i--) {
-                reversed += text.charAt(i);
+    public static void main(String[] args) {
+        System.out.println("Palindrome Checker App");
+        System.out.println();
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Input text: ");
+        String text = sc.nextLine();
+        char[] arr = text.toCharArray();
+        int left = 0;
+        int right = arr.length - 1;
+        boolean isPalindrome = true;
+        while (left < right) {
+            if (arr[left] != arr[right]) {
+                isPalindrome = false;
+                break;
             }
-            System.out.println("Reversed text: " + reversed);
-            if (text.equals(reversed)) {
-                System.out.println("Is it a Palindrome? : true");
-            } else {
-                System.out.println("Is it a Palindrome? : false");
-            }
+            left++;
+            right--;
         }
+        System.out.println("Is it a Palindrome? : " + isPalindrome);
     }
+}
